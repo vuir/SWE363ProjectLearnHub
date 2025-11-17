ad1cce16afd89a89518cc9c13f82b96cd9afd304
darkwinTech <s202257160@kfupm.edu.sa>
2025-11-17 13:39:42 +0300

    data to be used in courses

---
+++ src/data/courses.js
@@ -0,0 +1,15 @@
+import CalculateIcon from "@mui/icons-material/Calculate";
+import TerminalIcon from "@mui/icons-material/Terminal";
+
+export const courses = [
+  { id: "SWE 353",  title: "Web Engineering",         icon: <CalculateIcon/> },
+  { id: "ICS 104",  title: "Python",                  icon: <TerminalIcon/> },
+  { id: "ICS 500",  title: "Methodology",             icon: <TerminalIcon/> },
+  { id: "MATH 201", title: "Calculus 3",              icon: <CalculateIcon/> },
+  { id: "MATH 208", title: "Linear Algebra",          icon: <CalculateIcon/> },
+  { id: "COE 292",  title: "Digital Logic Design",    icon: <TerminalIcon/> },
+  { id: "IAS 430",  title: "Ethics & Society",        icon: <CalculateIcon/> },
+  { id: "COE 233",  title: "Computer Organization",   icon: <TerminalIcon/> },
+  { id: "SWE 216",  title: "Software Requirements",   icon: <CalculateIcon/> },
+  { id: "ICS 253",  title: "Discrete Structures",     icon: <TerminalIcon/> },
+];
